2d0171d9074a2aaf91302f27c6f4c2647f91bd9b
BryanHeBY <1319212443@qq.com>
2023-03-19 20:09:33 +0800

    Build v1.2

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,21 @@ RUN apt-get update
 RUN apt-get install -y build-essential libz-dev libncurses-dev \
         autoconf libtool pkg-config cmake python ninja-build git
 
-# build gRPC
+# Build llvm clang 9.0.1
+RUN apt-get install -y wget
+RUN mkdir -p /opt/llvm-source-build
+COPY llvm-src-archive /opt/llvm-source-build/llvm-src-archive
+RUN wget --directory-prefix /opt/llvm-source-build/llvm-src-archive \
+    https://github.com/llvm/llvm-project/releases/download/llvmorg-9.0.1/llvm-9.0.1.src.tar.xz \
+    https://github.com/llvm/llvm-project/releases/download/llvmorg-9.0.1/clang-9.0.1.src.tar.xz \
+    https://github.com/llvm/llvm-project/releases/download/llvmorg-9.0.1/compiler-rt-9.0.1.src.tar.xz \
+    https://github.com/llvm/llvm-project/releases/download/llvmorg-9.0.1/libcxx-9.0.1.src.tar.xz \
+    https://github.com/llvm/llvm-project/releases/download/llvmorg-9.0.1/libcxxabi-9.0.1.src.tar.xz \
+    https://github.com/llvm/llvm-project/releases/download/llvmorg-9.0.1/clang-tools-extra-9.0.1.src.tar.xz
+COPY llvm-clang-install.sh /opt/llvm-source-build
+RUN bash /opt/llvm-source-build/llvm-clang-install.sh
+
+# Build gRPC
 WORKDIR /opt
 RUN git clone --recurse-submodules -b v1.52.0 --depth 1 --shallow-submodules https://github.com/grpc/grpc
 RUN mkdir -p /opt/grpc/cmake/build
@@ -33,7 +47,7 @@ COPY --from=BUILDSTAGE /opt/grpc/examples /opt/grpc/examples
 
 RUN apt-get update
 RUN apt-get install -y build-essential libz-dev libncurses-dev \
-        cmake python ninja-build git
+        gdb cmake python ninja-build git
 
 # General version
 
---
+++ llvm-clang-install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+source_path_root=/opt/llvm-source-build/llvm-src-archive
+
+cd /opt/llvm-source-build
+cp ${source_path_root}/llvm-9.0.1.src.tar.xz .
+tar -xf llvm-9.0.1.src.tar.xz
+mv llvm-9.0.1.src llvm
+
+cd llvm/tools
+cp ${source_path_root}/clang-9.0.1.src.tar.xz .
+tar -xf clang-9.0.1.src.tar.xz
+mv clang-9.0.1.src clang
+
+cd clang/tools
+cp ${source_path_root}/clang-tools-extra-9.0.1.src.tar.xz .
+tar -xf clang-tools-extra-9.0.1.src.tar.xz
+mv clang-tools-extra-9.0.1.src extra
+
+cd ../../../projects  #now under the folder /llvm-source-build/llvm/projects
+cp ${source_path_root}/compiler-rt-9.0.1.src.tar.xz .
+tar -xf compiler-rt-9.0.1.src.tar.xz
+mv compiler-rt-9.0.1.src compiler-rt
+# git patch
+rm compiler-rt/lib/sanitizer_common/sanitizer_platform_limits_posix.h
+rm compiler-rt/lib/sanitizer_common/sanitizer_platform_limits_posix.cc
+cp ${source_path_root}/sanitizer_platform_limits_posix.h compiler-rt/lib/sanitizer_common/sanitizer_platform_limits_posix.h
+cp ${source_path_root}/sanitizer_platform_limits_posix.cc compiler-rt/lib/sanitizer_common/sanitizer_platform_limits_posix.cc
+
+
+cp ${source_path_root}/libcxx-9.0.1.src.tar.xz .
+tar -xf libcxx-9.0.1.src.tar.xz
+mv libcxx-9.0.1.src libcxx
+
+cp ${source_path_root}/libcxxabi-9.0.1.src.tar.xz .
+tar -xf libcxxabi-9.0.1.src.tar.xz
+mv libcxxabi-9.0.1.src libcxxabi
+
+cd ../..  #now under the folder /llvm-source-build
+mkdir llvm-build
+cd llvm-build
+cmake -G Ninja -DLLVM_TARGETS_TO_BUILD="X86;AArch64;RISCV" -DBUILD_SHARED_LIBS=ON \
+        -DLLVM_ENABLE_ASSERTIONS=On -DCMAKE_BUILD_TYPE=Release ../llvm
+ninja install
